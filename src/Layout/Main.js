581bb09ce95cf54c1dba66f78ebcac018ffa8d47
Tawhidul3233 <mdtawhidulislam3233@gmail.comm>
2022-11-09 12:14:56 +0600

    navbar and banner complete

---
+++ src/Layout/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Banner from '../Page/Home/Banner/Banner';
+import Footer from '../Page/Share/Footer/Footer';
+import Navbar from '../Page/Share/Navbar/Navbar';
+
+const Main = () => {
+     return (
+          <div>
+               <Navbar> </Navbar>
+               <Banner> </Banner>
+               <Footer> </Footer>
+          </div>
+     );
+};
+
+export default Main;
---
+++ src/Page/Home/Banner/Banner.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import img1 from '../../../assets/img1.jpg'
+import img2 from '../../../assets/img2.jpg'
+import img3 from '../../../assets/img3.jpg'
+import './Banner.css'
+
+const Banner = () => {
+     return (
+          <div>
+               <div className="carousel w-full">
+                    <div id="slide1" className="carousel-item relative w-full">
+                         <div className='img-gradient'>
+                              <img alt='' src={img1} className="w-full" />
+                         </div>
+                         <div className="absolute text-center w-4/5 md:w-2/5 md:text-right md:right-24  right-5   transform -translate-y-1/2   top-1/4">
+                              <h1 className='text-3xl font-bold text-white'>
+                                   Lowest cost car rent where you<br /> want when    you want </h1>
+                         </div>
+                         <div className="absolute text-center w-4/5 md:w-2/5 md:text-right md:right-24  right-5  transform -translate-y-1/2 top-1/2">
+                              <p className='text-xl text-white'>
+                              To start the easy process of booking a car rental on Expedia, simply enter the location along with the dates and times for pickup and drop-off.
+                              </p>
+                         </div>
+                         <div className="absolute flex md:right-24 right-1/3 transform -translate-y-1/2  top-3/4">
+                              <button className="btn btn-warning">Click Here</button>
+                         </div>
+                         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 bottom-3">
+                              <a href="#slide3" className="btn btn-circle">❮</a>
+                              <a href="#slide2" className="btn btn-circle">❯</a>
+                         </div>
+                    </div>
+                    <div id="slide2" className="carousel-item relative w-full">
+                    <div className='img-gradient'>
+                              <img alt='' src={img2} className="w-full" />
+                         </div>
+                         <div className="absolute text-center w-4/5 md:w-2/5 md:text-right md:right-24  right-5   transform -translate-y-1/2   top-1/4">
+                              <h1 className='text-3xl font-bold text-white'>
+                                   We will deliver The  <br /> Car to your door </h1>
+                         </div>
+                         <div className="absolute flex text-center w-4/5 md:w-2/5 md:text-right md:right-24  right-5 transform -translate-y-1/2   top-1/2">
+                              <p className='text-xl text-white'>
+                              Booking car rentals with Expedia makes it easy to find the right rental and book the rest of your trip in a single, convenient location. We also offer flights, hotels, and even activities you can book in advance
+                              </p>
+                         </div>
+                         <div className="absolute flex transform -translate-y-1/2 md:right-24 right-1/3  top-3/4">
+                              <button className="btn btn-warning ">Click Here</button>
+                         </div>
+                         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 bottom-3">
+                              <a href="#slide1" className="btn btn-circle">❮</a>
+                              <a href="#slide3" className="btn btn-circle">❯</a>
+                         </div>
+                    </div>
+                    <div id="slide3" className="carousel-item relative w-full">
+                    <div className='img-gradient'>
+                              <img alt='' src={img3} className="w-full" />
+                         </div>
+                         <div className="absolute text-center w-4/5 md:w-2/5 md:text-right md:right-24  right-5   transform -translate-y-1/2  top-1/4">
+                              <h1 className='text-3xl font-bold text-white'>
+                                   The car will take you <br /> to your destination </h1>
+                         </div>
+                         <div className="absolute flex text-center w-4/5 md:w-2/5 md:text-right md:right-24  right-5  transform -translate-y-1/2  top-1/2">
+                              <p className='text-xl text-white'>
+                              Some car rentals are fully refundable. Each one that qualifies will be marked as such in green text on the search results page. You may also filter for this feature specifically.
+                              </p>
+                         </div>
+                         <div className="absolute flex transform -translate-y-1/2 md:right-24 right-1/3 top-3/4">
+                              <button className="btn btn-warning ">Click Here</button>
+                         </div>
+                         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 bottom-3">
+                              <a href="#slide2" className="btn btn-circle">❮</a>
+                              <a href="#slide1" className="btn btn-circle">❯</a>
+                         </div>
+                    </div>
+               </div>
+          </div>
+     );
+};
+
+export default Banner;
