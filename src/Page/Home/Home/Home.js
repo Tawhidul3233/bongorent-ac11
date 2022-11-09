2317f2792d34cca793a6438e22f039e9d1faa4e3
Tawhidul3233 <mdtawhidulislam3233@gmail.comm>
2022-11-09 20:50:47 +0600

    added another section display upcoming service

--- src/Page/Home/Home/Home.js
+++ src/Page/Home/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import SiteStatus from '../SiteStatus/SiteStatus';
+import UpcomingServices from '../UpcomingServices/UpcomingServices';
 
 const Home = () => {
      return (
           <div>
                <Banner> </Banner>
                <SiteStatus> </SiteStatus>
+               <UpcomingServices> </UpcomingServices>
           </div>
      );
 };
---
+++ src/Page/Home/UpcomingServices/UpcomingServices.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+const UpcomingServices = () => {
+     return (
+          <div>
+               <h2 className='text-center text-4xl font-semibold my-5'>Display upcoming Services</h2>
+               <p className='mx-auto text-center w-3/5'> We will start Biman Service and Launch Service . You Want to Know Dhaka to Barishal Launch Ticket Price or Dhaka to Barishal Launch Schedule? Yes! you get all information about Barishal to Dhaka launch schedule in this post.</p>
+               <div className='grid md:grid-cols-2 gap-5 m-5'>
+                    <div className=' border-2 rounded-md'>
+                         <img src="https://i.ibb.co/Tr6mNkV/biman.jpg" alt="" />
+                         <h3>Biman Service </h3>
+                         <p className='mx-auto text-center w-3/5'> We will start Biman Service and Launch Service . You Want to Know Dhaka to Barishal Launch Ticket Price or Dhaka to Barishal Launch Schedule? Yes! you get all information about Barishal to Dhaka launch schedule in this post.</p>
+                    </div>
+                    <div>
+                         <img src="https://i.ibb.co/QHLm1cJ/lonce.jpg" alt="" />
+                         <h3>Biman Service </h3>
+                         <p className='mx-auto text-center w-3/5'> We will start Biman Service and Launch Service . You Want to Know Dhaka to Barishal Launch Ticket Price or Dhaka to Barishal Launch Schedule? Yes! you get all information about Barishal to Dhaka launch schedule in this post.</p>
+                    </div>
+               </div>
+          </div>
+     );
+};
+
+export default UpcomingServices;
