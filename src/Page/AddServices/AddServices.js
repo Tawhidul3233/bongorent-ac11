b0d8ab11eedc16b791171e09ff8ceec60b143e3c
Tawhidul3233 <mdtawhidulislam3233@gmail.comm>
2022-11-10 12:32:18 +0600

    new services add successfully

--- src/Page/AddServices/AddServices.js
+++ src/Page/AddServices/AddServices.js
@@ -1,4 +1,6 @@
+import { fromJSON } from 'postcss';
 import React from 'react';
+import toast, { Toaster } from 'react-hot-toast';
 
 const AddServices = () => {
 
@@ -13,7 +15,32 @@ const AddServices = () => {
           const thumbnail_url = form.thumbnail_url.value;
           const email = form.email.value;
           const details = form.details.value;
-          console.log(title, price, Booked, available, thumbnail_url, details, email)
+
+          const services={
+               title,
+               price,
+               Booked,
+               available,
+               thumbnail_url,
+               email,
+               details,
+          }
+          console.log(services)
+          fetch('http://localhost:5000/allservices',{
+               method: 'POST',
+               headers:{
+                    'content-type':"application/json"
+               },
+               body: JSON.stringify(services)
+          })
+          .then(res => res.json())
+          .then(data => {
+               console.log(data)
+               toast.success('Service add successfylly Go all services option and check')
+               form.reset()
+          })
+          .catch(err => console.log(err))
+
      }
 
 
@@ -22,9 +49,9 @@ const AddServices = () => {
                <h1 className='my-5 text-3xl font-semibold'> Create New Services</h1>
                <form onSubmit={serviceSubmitHandler} className='grid  gap-4' action="">
                     <d className='grid gap-4 md:grid-cols-2'>
-                         <input className=' border-2 p-2' name='title' type="text" placeholder='Services Name' />
+                         <input className=' border-2 p-2' name='title' type="text" placeholder='Services Name' required />
 
-                         <input className=' border-2 p-2' name='price' type="text" placeholder='Price$' />
+                         <input className=' border-2 p-2' name='price' type="text" placeholder='Price$'  required />
 
                          <input className=' border-2 p-2' name='Booked' type="text" placeholder='Booked' />
 
@@ -32,7 +59,7 @@ const AddServices = () => {
 
                          <input className=' border-2 p-2' name='thumbnail_url' type="text" placeholder='Thumbnail_url' />
 
-                         <input className=' border-2 p-2' name='email' type="text" placeholder='Email' />
+                         <input className=' border-2 p-2' name='email' type="text" placeholder='Email' required />
 
                     </d>
                     <textarea className='border-2 p-4' name="details" id="" cols="30" rows="10" placeholder='Details'></textarea>
--- src/Page/Register/Register.js
+++ src/Page/Register/Register.js
@@ -82,13 +82,13 @@ const Register = () => {
                                         <label className="label">
                                              <span className="label-text">Email</span>
                                         </label>
-                                        <input type="text" name='email' placeholder="email" className="input input-bordered" />
+                                        <input type="text" name='email' placeholder="email" className="input input-bordered" required />
                                    </div>
                                    <div className="form-control">
                                         <label className="label">
                                              <span className="label-text">Password</span>
                                         </label>
-                                        <input type="text" name='password' placeholder="password" className="input input-bordered" />
+                                        <input type="text" name='password' placeholder="password" className="input input-bordered" required />
                                         <label className="label">
 
                                         </label>
