4c4e9be460f3993393344bf31e8f6f773e75bb6a
Tawhidul3233 <mdtawhidulislam3233@gmail.comm>
2022-11-10 10:05:59 +0600

    home page services scection done

--- src/Page/Home/HomeServices/HomeServices.js
+++ src/Page/Home/HomeServices/HomeServices.js
@@ -1,5 +1,6 @@
 import { linkWithCredential } from 'firebase/auth';
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import Service from '../Service/Service';
 
 
@@ -16,15 +17,20 @@ const HomeServices = () => {
 
 
      return (
-          <div>
+          <div className=''>
                <h1 className='text-center text-4xl font-semibold my-5'>My Service Area</h1>
-               <div>
+               <div className='grid md:grid-cols-3 gap-5 '>
                     {
                          services.map(service => <Service
-                         key={service._id}
-                         service={service}
+                              key={service._id}
+                              service={service}
                          ></Service>)
                     }
+               </div>
+               <div className='text-center my-5'>
+                    <Link to='/allservices'>
+                         <button className="btn btn-primar mb-5 px-5">See all Services</button>
+                    </Link>
                </div>
           </div>
      );
--- src/Page/Home/Service/Service.js
+++ src/Page/Home/Service/Service.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import { FaForward } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
-const Service = ({service}) => {
-     const {title, _id, details, available, booked} = service
-    
+const Service = ({ service }) => {
+     const { title, _id, rating, details, price, available, Booked, thumbnail_url } = service
+     const {} =rating
+
      return (
-          <div>
-              {title}
+          <div className='my-5'>
+               <div className="card card-compact w-96 bg-base-100 shadow-xl">
+                    <figure><img src={thumbnail_url} alt="Shoes" /></figure>
+                    <div className="card-body">
+                         <div className='flex text-end items-center mb-3'>
+                              <h2 className="card-title">{title}</h2>
+                              <p className='text-2xl font-semibold text-blue-500'>{price}</p>
+                         </div>
+                         <p>{details.slice(0, 100)}...</p>
+                         <div className="card-actions justify-end flex items-center my-3">
+                              <p className=''> Available : {available}</p>
+                              <p className=''> Booked : {Booked}</p>
+                              <Link to="/servicesdetails">
+                                   <button className="btn btn-primary">View Details <FaForward className='ml-2'> </FaForward> </button>
+                              </Link>
+                         </div>
+                    </div>
+               </div>
           </div>
      );
 };
